71864835b37ba3eeb1e7f2a2a60ab6ae76952243
Strife-Cyber <dunamisjunior15@gmail.com>
2025-06-30 14:10:22 +0100

    no ports

--- main.py
+++ main.py
@@ -48,5 +48,6 @@ async def run_inference(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3002, reload=True)
+    # uvicorn.run("main:app", host="0.0.0.0", port=3002, reload=True)
+    uvicorn.run("main:app")
 
