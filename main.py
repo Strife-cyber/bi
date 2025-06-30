66c522788ad405692d37604c578bb26a521d13a3
Strife-Cyber <dunamisjunior15@gmail.com>
2025-06-30 14:23:48 +0100

    pls workj

--- main.py
+++ main.py
@@ -54,5 +54,5 @@ async def run_inference(
 if __name__ == "__main__":
     import uvicorn
     # uvicorn.run("main:app", host="0.0.0.0", port=3003, reload=True)
-    uvicorn.run("main:app")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
